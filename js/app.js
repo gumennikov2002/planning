5a301785b2abe1fd5036f1b8166ef5e7f65e432c
gumennikov2002 <gumennikov2002@yandex.ru>
2022-04-05 19:24:15 +0400

    Html template was made

---
+++ js/app.js
@@ -0,0 +1,10 @@
+class App {
+    constructor() {
+        this.app = document.getElementById('app');
+        this.init();
+    }
+
+    init = () => {
+        console.log('App is running');
+    }
+}
